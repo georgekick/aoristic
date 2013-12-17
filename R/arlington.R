94640f870804e4afb36e022edc02183006ecf524
unknown <gkikuchi@gkikuchi-win.ad.csufresno.edu>
2013-12-16 22:18:59 -0800

    data loading successful (automatic/attached) data described in aoristic-package.r

--- R/arlington.R
+++
@@ -1,8 +0,0 @@
-#' @name arlington
-#' @title arlington burglary incident data (data frame)
-#' @description A data frame of burglary incidents in the Arlington PD with four fields (DateTimeFrom, DateTimeTo, lon/lat).
-#' @docType data
-#' @usage arlington
-#' @source Arlington PD 
-#' @author George Kikuchi, 2013-09-13
-NULL
