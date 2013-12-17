3eafc9a2b16c25d2f89b78d2efa852f298917503
unknown <gkikuchi@gkikuchi-win.ad.csufresno.edu>
2013-12-16 20:02:49 -0800

    data documentation added error with .Random.seed and oldLC

---
+++ R/CouncilDistrict.r
@@ -0,0 +1,8 @@
+#' @name CouncilDistrict
+#' @title Council district (spatial polygon data frame)
+#' @description A spatial polygon data frame of council districts in the Arlington PD jurisdiction.
+#' @docType data
+#' @usage CouncilDistrict
+#' @source City of Arlington 
+#' @author George Kikuchi, 2013-09-13
+NULL
